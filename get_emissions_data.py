b45ed7d4502c1b8fb8ad3cd27944f1e789fbcbca
Jonathan Veys <Jonathancr1234@gmail.com>
2025-06-22 23:07:11 +0100

    Built function to fetch emissions data using the climate trace api

---
+++ get_emissions_data.py
@@ -0,0 +1,26 @@
+import requests
+
+
+def get_emissions_data(year:int)->dict: 
+    url = "https://api.climatetrace.org/v6/country/emissions"
+
+    params = {
+        "sectors": "power,transportation,buildings,fossil-fuel-operations,manufacturing,mineral-extraction,agriculture,waste,fluorinated-gases,forestry-and-land-use",
+        "groups":'unfccc_nonannex1,unfccc_annex1',
+        "since": year,
+        "to": year
+    }
+
+    response = requests.get(url, params=params)
+
+    if response.status_code == 200:
+        emissions_data = {
+            country['country']: country['emissions']['co2']
+            for country in response.json()
+        } 
+    else:
+        print(f"Error {response.status_code}: {response.text}")
+        
+    return emissions_data
+
+print(get_emissions_data(2015)['GBR'])
